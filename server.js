6ad268eb62d31b1f6f378f9a701c378a74ee1b3d
Nadim Ayaz <nadimayaz@gmail.com>
2018-01-10 00:28:24 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+
+
+var express    = require('express');
+var bodyParser = require('body-parser');
+var sql        = require('mysql');
+
+var app	       = express();
+app.listen(port;
+console.log('server is running @',port);
+
+app.get('/',function(req,res){
+	
+});
